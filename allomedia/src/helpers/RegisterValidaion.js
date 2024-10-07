98e88e2bfbdb2e01989ed079c9ae63e21ff281df
e-lglioui <elgliouiyoucode@gmail.com>
2024-10-07 10:41:44 +0200

    validatiopn de forme register

---
+++ allomedia/src/helpers/RegisterValidaion.js
@@ -0,0 +1,8 @@
+import nameVerify from InputVerify;
+import emailVerify from InputVerify;
+import passwordVerify from InputVerify;
+import  phoneNumberVerify from InputVerify;
+
+export async function RegisterValidation (valeus){
+
+}
