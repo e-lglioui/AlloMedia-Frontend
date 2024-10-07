4393176af7ce0ed793e5d0d5e43fc51166dc2ebd
e-lglioui <elgliouiyoucode@gmail.com>
2024-10-07 17:23:22 +0200

    la fonction forgetpassword

--- allomedia/src/pages/forgetPassword.jsx
+++ allomedia/src/pages/forgetPassword.jsx
@@ -10,7 +10,7 @@ const ForgetPassword = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post(`${process.env.REACT_APP_API_URL}/forgetPassword`, { email });
+      const response = await axios.post(`http://localhost:3000/api/auth/forgetpassword`, { email });
       setMessage(response.data.message);
       setError(''); 
     } catch (err) {
